1811ef90cbeb4962d8d8b93cd9de181d52049c01
Antonio Trifirò <antonio.trifiro@davincimilazzo.edu.it>
2023-04-04 09:25:51 +0200

    fix missing bracket

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ class ShellSettings:
     
     CONTRIBUTOR(s):
     * Antonio Caristia (antonio.caristia at davincimilazzo.edu.it)
-    * Antonio Trifirò (antonio.trifiro at davincimilazzo.edu.it
+    * Antonio Trifirò (antonio.trifiro at davincimilazzo.edu.it)
     """
     USERNAME = None
     PASSWORD = None
