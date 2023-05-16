8c31553d117c5c16abaab7dc56b5c3c38102a7ea
antonio <antonio.caristia@davincimilazzo.edu.it>
2023-05-16 09:19:22 +0200

    Fix missing logoutresponse in factory

--- server_response/server_responses_factory.py
+++ server_response/server_responses_factory.py
@@ -1,12 +1,15 @@
 from message.messages_codes import LOGIN
+from message.messages_codes import LOGOUT
 from message.messages_codes import REGISTER
 from .login_response import LoginResponse
 from .register_response import RegisterResponse
+from .logout_response import LogoutResponse
 
 
 class ServerResponsesFactory:
     MESSAGES = {
         LOGIN : LoginResponse,
+        LOGOUT: LogoutResponse,
         REGISTER : RegisterResponse,
     }
 
