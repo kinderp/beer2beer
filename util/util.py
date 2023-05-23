f79989abe5cf445ec3d5216cb2d458232c79c195
Michael Basile <michael.basile@davincimilazzo.edu.it>
2023-05-23 09:26:38 +0200

    added list of files

--- util/util.py
+++ util/util.py
@@ -1,4 +1,5 @@
-import os
+import os.path
+from os import listdir, stat
 import random
 
 from server.storage import Storage
@@ -13,3 +14,15 @@ class Util:
     @classmethod
     def generate_userid(self):
         return str(Storage.free_ids.pop())
+
+    def browse_dir(self, dir_name):
+        list_of_files=listdir(dir_name)
+        for elem in list_of_files:
+            dim=stat(elem)
+            tmp={}
+            tmp[elem]=dim
+            list_of_files.append(tmp)
+        return list_of_files
+
+
+
