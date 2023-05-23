f00de31e5a6757eac382bd5f235d1d5a629da615
Antonio Trifirò <antonio.trifiro@davincimilazzo.edu.it>
2023-05-23 09:19:28 +0200

    added receiver register update

---
+++ receiver/receiver_register_update.py
@@ -0,0 +1,21 @@
+from message.messages_codes import REGISTER
+from message.messages_factory import MessagesFactory
+from util.session import Session
+
+
+class ReceiverRegisterUpdate:
+    def __init__(self, host, port, payload):
+        self._host = host
+        self._port = port
+        self._payload = payload
+
+    def register_update(self, args=None):
+        register_update_message = MessagesFactory.create(
+                message_type=REGISTER,
+                data=self._payload
+        )
+        session = Session(self._host, self._port)
+        session.connect()
+        session.send_message(register_update_message)
+        response = session.recv_message()
+        return response
