8d4cfd8c03758ad3712a66068a6dd224d62119b1
Michael Basile <michael.basile@davincimilazzo.edu.it>
2023-05-25 17:27:32 +0200

    Add sha1 in Util

--- util/util.py
+++ util/util.py
@@ -1,7 +1,8 @@
 import os.path
-from os import listdir, stat
+from os import listdir, stat, path
 import random
-
+import hashlib
+from pathlib import Path
 from server.storage import Storage
 from server.storage import Content
 
@@ -21,11 +22,22 @@ class Util:
         results = []
         list_of_files=listdir(dir_name)
         for elem in list_of_files:
+            if path.isdir(Path(dir_name, elem)): continue
             dim=stat(elem).st_size
-            # TODO add sha1
-            tmp = Content(elem, dim, "...")
+            sha1 = cls.sha1sum(dir_name, elem)
+            tmp = Content(elem, dim, sha1)
             results.append(tmp)
         return results
 
-
-
+    @classmethod
+    def sha1sum(cls, dir_name, file_name):
+        BUF_SIZE = 65536
+        sha1 = hashlib.sha1()
+        absolute_filename = Path(dir_name, file_name)
+        with open(absolute_filename, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha1.update(data)
+        return sha1.hexdigest()
