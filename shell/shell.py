266a040393762d941b4970f391f778d1f1a60174
kinderp <a.caristia@gmail.com>
2024-11-30 11:31:15 +0000

    fix None port in util/session.py

--- shell/shell.py
+++ shell/shell.py
@@ -135,9 +135,8 @@ class Beer2BeerShell(cmd.Cmd):
     def get_peer_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
+        ip, port = s.getsockname()
         s.close()
-        print("hostname: ", ip)
         return ip
 
     def do_login(self, arg):
